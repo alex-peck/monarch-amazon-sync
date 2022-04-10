694c957a97eced0b06b1737f1253d040c0eb6a5b
Alex Peck <alex-peck@github.com>
2022-04-10 20:31:39 +0900

    template init

---
+++ manifest.js
@@ -0,0 +1,48 @@
+import packageJson from './package.json' assert { type: 'json' };
+
+/**
+ * After changing, please reload the extension at `chrome://extensions`
+ * @type {chrome.runtime.ManifestV3}
+ */
+const manifest = {
+  manifest_version: 3,
+  name: packageJson.name,
+  version: packageJson.version,
+  description: packageJson.description,
+  permissions: ['storage', 'sidePanel'],
+  side_panel: {
+    default_path: 'src/pages/sidepanel/index.html',
+  },
+  options_page: 'src/pages/options/index.html',
+  background: {
+    service_worker: 'src/pages/background/index.js',
+    type: 'module',
+  },
+  action: {
+    default_popup: 'src/pages/popup/index.html',
+    default_icon: 'icon-34.png',
+  },
+  chrome_url_overrides: {
+    newtab: 'src/pages/newtab/index.html',
+  },
+  icons: {
+    128: 'icon-128.png',
+  },
+  content_scripts: [
+    {
+      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+      js: ['src/pages/content/index.js'],
+      // KEY for cache invalidation
+      css: ['assets/css/contentStyle<KEY>.chunk.css'],
+    },
+  ],
+  devtools_page: 'src/pages/devtools/index.html',
+  web_accessible_resources: [
+    {
+      resources: ['assets/js/*.js', 'assets/css/*.css', 'icon-128.png', 'icon-34.png'],
+      matches: ['*://*/*'],
+    },
+  ],
+};
+
+export default manifest;
---
+++ vite.config.ts
@@ -0,0 +1,92 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path, { resolve } from 'path';
+import makeManifest from './utils/plugins/make-manifest';
+import customDynamicImport from './utils/plugins/custom-dynamic-import';
+import addHmr from './utils/plugins/add-hmr';
+import watchRebuild from './utils/plugins/watch-rebuild';
+
+const rootDir = resolve(__dirname);
+const srcDir = resolve(rootDir, 'src');
+const pagesDir = resolve(srcDir, 'pages');
+const assetsDir = resolve(srcDir, 'assets');
+const outDir = resolve(rootDir, 'dist');
+const publicDir = resolve(rootDir, 'public');
+
+const isDev = process.env.__DEV__ === 'true';
+const isProduction = !isDev;
+
+// ENABLE HMR IN BACKGROUND SCRIPT
+const enableHmrInBackgroundScript = true;
+const cacheInvalidationKeyRef = { current: generateKey() };
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@root': rootDir,
+      '@src': srcDir,
+      '@assets': assetsDir,
+      '@pages': pagesDir,
+    },
+  },
+  plugins: [
+    makeManifest({
+      getCacheInvalidationKey,
+    }),
+    react(),
+    customDynamicImport(),
+    addHmr({ background: enableHmrInBackgroundScript, view: true }),
+    isDev && watchRebuild({ afterWriteBundle: regenerateCacheInvalidationKey }),
+  ],
+  publicDir,
+  build: {
+    outDir,
+    /** Can slow down build speed. */
+    // sourcemap: isDev,
+    minify: isProduction,
+    modulePreload: false,
+    reportCompressedSize: isProduction,
+    emptyOutDir: !isDev,
+    rollupOptions: {
+      input: {
+        devtools: resolve(pagesDir, 'devtools', 'index.html'),
+        panel: resolve(pagesDir, 'panel', 'index.html'),
+        content: resolve(pagesDir, 'content', 'index.ts'),
+        background: resolve(pagesDir, 'background', 'index.ts'),
+        contentStyle: resolve(pagesDir, 'content', 'style.scss'),
+        popup: resolve(pagesDir, 'popup', 'index.html'),
+        newtab: resolve(pagesDir, 'newtab', 'index.html'),
+        options: resolve(pagesDir, 'options', 'index.html'),
+        sidepanel: resolve(pagesDir, 'sidepanel', 'index.html'),
+      },
+      output: {
+        entryFileNames: 'src/pages/[name]/index.js',
+        chunkFileNames: isDev ? 'assets/js/[name].js' : 'assets/js/[name].[hash].js',
+        assetFileNames: assetInfo => {
+          const { name } = path.parse(assetInfo.name);
+          const assetFileName = name === 'contentStyle' ? `${name}${getCacheInvalidationKey()}` : name;
+          return `assets/[ext]/${assetFileName}.chunk.[ext]`;
+        },
+      },
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['**/*.test.ts', '**/*.test.tsx'],
+    setupFiles: './test-utils/vitest.setup.js',
+  },
+});
+
+function getCacheInvalidationKey() {
+  return cacheInvalidationKeyRef.current;
+}
+function regenerateCacheInvalidationKey() {
+  cacheInvalidationKeyRef.current = generateKey();
+  return cacheInvalidationKeyRef;
+}
+
+function generateKey(): string {
+  return `${Date.now().toFixed()}`;
+}
